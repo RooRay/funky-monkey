6ad4a314dd7dc251bc54c30bb9f1193cd0655ac0
RooRay <86845749+RooRay@users.noreply.github.com>
2022-01-16 19:38:29 +0000

    added example files, fixed command handler and made ping command work

---
+++ example files/command-example.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'command-name', // this name will be referenced in index.js
+	description: "ping command", // doesn't really matter
+	execute(message, args){
+			// paste code here
+	}
+}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const Discord = require("discord.js")
+const fs = require('fs');
 const app = express();
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
 app.listen(3000, () => {
   console.log("Bot is functioning and online!")
@@ -9,38 +12,32 @@ app.get("/", (req, res) => {
   res.send ("Hello! If you see this screen that means the bot is functioning!:D");
 })
 
-const Discord = require("discord.js")
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+client.commands = new Discord.Collection();
 
-client.on("messageCreate", message => {
-  if(message.content === "ping") {
-    message.channel.send("pong!")
-  }
-	if(message.content === "testembed") {
-		let embed = new Discord.MessageEmbed()
-		.setTitle("Embed Test")
-		.setURL("https://redirects.rooray.link/u/gWOb3o6Z")
-		.setDescription("Hiya! This is just a test embed to make sure this new, DiscordJS 13 bot works fully! The bot is gonna have lots of fun and cool features soon. ")
-		.addField("I will", "stomp your kneecaps backwards")
-		.setThumbnail(message.author.displayAvatarURL)
-		.setFooter("Psst! The colour on the side of this message changes every time you run it!")
-		.setColor("RANDOM")
-		.setTimestamp()
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+	const command = require(`./commands/${file}`);
 
-		message.channel.send({embeds:[embed], content: "Your wish is my command!"})
-	}
-	if(message.content === "build") {
-		let embed = new Discord.MessageEmbed()
-		.setTitle("Build Information")
-		.addField("Build Variant (Dev or Stable)", "Dev")
-		.addField("Build Version", "1.0.1")
-		.addField("Build Released", "14th January 2022")
-		.addField("Build Changelog", "Added some smaller commands and fixed some things.")
-		.setColor("RED")
-		.setTimestamp()
+	client.commands.set(command.name, command);
+}
 
-		message.channel.send({embeds:[embed], content: "All you need to know is below!"})
-	}
+app.get("/", (req, res) => {
+  res.send ("Bot is working!");
 })
+
+const prefix = '%';
+
+client.on('message', message => {
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if(command === 'ping'){
+		client.commands.get('ping').execute(message, args);
+	} else if (command == 'dingus') {
+		message.channel.send('dingus')
+	}
+});
 
 client.login(process.env.token)
